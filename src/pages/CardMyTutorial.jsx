611b01a5c25daa06a41713cdc1445900815d0717
habibullah <habibullahkhandokar01@gmail.com>
2025-01-19 23:36:38 +0600

    successflly add delete button

--- src/pages/CardMyTutorial.jsx
+++ src/pages/CardMyTutorial.jsx
@@ -3,10 +3,44 @@ import { FaHeart } from "react-icons/fa6";
 import { IoTrashBin } from "react-icons/io5";
 import { BiEdit } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const CardMyTutorial = ({ data }) => {
     const {name, image, description, language, price, review, _id} = data;
-
+const handleDelete = id =>{
+  console.log(id);
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      fetch(`${import.meta.env.VITE_SERVER_url}/delete/${id}`,{
+        method: 'DELETE'
+    })
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data);
+        if(data.deletedCount > 0){
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          }).then(()=>{
+            window.location.reload();
+          })
+          .catch((err) => console.error("Error deleting tutorial:", err));
+      console.log("delete confirm");
+        }
+      })
+      console.log('delete confirm');
+    }
+  });
+}
   return (
   <div className="card card-side bg-base-100 shadow-xl max-w-[600px] max-h-[700px] min-h-[225px] border-2 p-2 grid grid-cols-5">
   <figure className="col-span-1">
@@ -52,8 +86,10 @@ const CardMyTutorial = ({ data }) => {
 
  {/*updatae and delete butotn*/}
  <div className=" col-span-5 flex gap-4 items-baseline justify-end p-2 text-xl">
-  <button className="text-red-600"><IoTrashBin /></button>
-  <Link to={`/UpdateMyTutorial/${_id}`}><button className="text-lime-600"><BiEdit /></button></Link>
+  <button
+  onClick={()=>handleDelete(_id)}
+  className="text-red-600"><IoTrashBin /></button>{/*delete button*/}
+  <Link to={`/UpdateMyTutorial/${_id}`}><button className="text-lime-600"><BiEdit /></button></Link>{/*edit/update button*/}
  </div>
 </div>
   );
